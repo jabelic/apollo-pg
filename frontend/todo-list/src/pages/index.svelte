f6f77e4ed60202e6a6763521f9e6f9060cd54aab
Jabelic <hibiki990523@gmail.com>
2021-10-02 22:15:58 +0900

    add: backend setup,frontend svelte split component

---
+++ frontend/todo-list/src/pages/index.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import List from "../template/List.svelte";
+  export let task = [];
+  export let username = "jabelic";
+</script>
+
+<main>
+  <h1>Hello {username}!</h1>
+  <h2>TODO List</h2>
+  <List />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    /* text-transform: uppercase; */
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ frontend/todo-list/src/template/List.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  export const req = () => {
+    console.debug("200");
+  };
+</script>
+
+<main>
+  <div>
+    絞り込み:
+    <button on:click={req}>All</button>
+    <!-- <button
+          on:click={() => {
+            condition = false;
+          }}>Undone</button
+        >
+        <button
+          on:click={() => {
+            condition = true;
+          }}>Done</button
+        > -->
+  </div>
+  <div>
+    <!-- <input type="text" bind:value={currentText} bind:this={initFocus} />
+        <button on:click={addTask}>Submit</button> -->
+  </div>
+  <div>
+    <ul>
+      <!-- {#each filterTodoList() as todo (todo.id)}
+                {#key todo.done}
+                <li>
+                    <input type="checkbox" bind:checked={todo.done}>{todo.title}
+                </li>
+                {/key}
+                {/each} -->
+    </ul>
+  </div>
+</main>
+
+<style></style>
